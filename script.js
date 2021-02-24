c35056147b5b3b9725c4d78657da59307068faa7
Nicki Patrick Graczyk <72356768+ngraczyk@users.noreply.github.com>
2021-02-24 01:26:15 +0100

    version 1 with images

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //canvas setup
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 500;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 let score = 0;
 let gameFrame = 0;
-ctx.font = '50px Georgia';
+ctx.font = canvas.width / 20 + 'px Georgia';
+let gameSpeed = 1;
+let gameOver = false;
 
 //mouse interactivity
 let canvasPosition = canvas.getBoundingClientRect();
@@ -51,12 +53,26 @@ class Player {
     let theta = Math.atan2(dy, dx);
     this.angle = theta;
 
+    if (gameFrame % 5 == 0) {
+      this.frame++;
+      if (this.frame >= 12) this.frame = 0;
+      if (this.frame == 3 || this.frame == 7 || this.frame == 11) {
+        this.frameX = 0;
+      } else {
+        this.frameX++;
+      }
+      if (this.frame < 3) this.frameY = 0;
+      else if (this.frame < 7) this.frameY = 1;
+      else if (this.frame < 11) this.frameY = 2;
+      else this.frameY = 0;
+    }
+
     if (mouse.x != this.x) {
-      this.x -= dx/30;
+      this.x -= dx / 30;
     }
 
     if (mouse.y != this.y) {
-      this.y -= dy/30;
+      this.y -= dy / 30;
     }
   }
   draw() {
@@ -68,12 +84,12 @@ class Player {
       ctx.lineTo(mouse.x, mouse.y);
       ctx.stroke();
     }
-    ctx.fillStyle = 'red';
+    /*ctx.fillStyle = 'red';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
-    ctx.fillRect(this.x, this.y, this.radius, 10);
+    ctx.fillRect(this.x, this.y, this.radius, 10);*/
 
     ctx.save();
     ctx.translate(this.x, this.y);
@@ -94,6 +110,8 @@ const player = new Player();
 
 //bubbles
 const bubblesArray = [];
+const bubbleImage = new Image();
+bubbleImage.src = 'bubble_pop_frame_01.png';
 
 class Bubble {
   constructor() {
@@ -108,15 +126,16 @@ class Bubble {
     this.y -= this.speed;
     const dx = this.x - player.x;
     const dy = this.y - player.y;
-    this.distance = Math.sqrt(dx*dx + dy*dy);
+    this.distance = Math.sqrt(dx * dx + dy * dy);
   }
   draw() {
-    ctx.fillStyle = 'blue';
+    /*ctx.fillStyle = 'blue';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
-    ctx.stroke();
+    ctx.stroke();*/
+    ctx.drawImage(bubbleImage, this.x - 65, this.y - 65, this.radius * 2.6, this.radius * 2.6);
   }
 }
 
@@ -128,19 +147,20 @@ function handleBubbles() {
   for (let i = 0; i < bubblesArray.length; i++) {
     bubblesArray[i].update();
     bubblesArray[i].draw();
-  }
 
-  for (let i = 0; i < bubblesArray.length; i++) {
     if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
       bubblesArray.splice(i, 1);
+      i--;
       continue;
     }
 
     if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
       sounds[bubblesArray[i].sound].play();
       bubblesArray.splice(i, 1);
+      i--;
       score++;
     }
+
   }
 }
 
@@ -148,16 +168,123 @@ const sounds = [document.createElement('audio'), document.createElement('audio')
 sounds[1].src = 'Plop.ogg';
 sounds[0].src = 'bubbles-single1.wav';
 
+//Repeating backgrounds
+const background = new Image();
+background.src = 'background1.png';
+
+const BG = {
+  x1: 0,
+  x2: canvas.width,
+  y: 0,
+  width: canvas.width,
+  height: canvas.height
+}
+
+function handleBackground() {
+  BG.x1 -= gameSpeed;
+  if (BG.x1 < -BG.width) {
+    BG.x1 = BG.width;
+  }
+  BG.x2 -= gameSpeed;
+  if (BG.x2 < -BG.width) {
+    BG.x2 = BG.width;
+  }
+  ctx.drawImage(background, BG.x1, BG.y, BG.width, BG.height);
+  ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height);
+}
+
+//enemie
+const enemyImage = new Image();
+enemyImage.src = 'enemy1.png';
+
+class Enemy {
+  constructor() {
+    this.x = canvas.width - 200;
+    this.y = Math.random() * (canvas.height - 150) + 90;
+    this.radius = 60;
+    this.speed = Math.random() * 2 + 2;
+    this.frame = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.spriteWidth = 418;
+    this.spriteHeight = 397;
+  }
+  update() {
+    this.x -= this.speed;
+    if (this.x < 0 - this.radius * 2) {
+      this.x = canvas.width + 200;
+      this.y = Math.random() * (canvas.height - 150) + 90;
+      this.speed = Math.random() * 2 + 2;
+    }
+    if (gameFrame % 5 == 0) {
+      this.frame++;
+      if (this.frame >= 12) this.frame = 0;
+      if (this.frame == 3 || this.frame == 7 || this.frame == 11) {
+        this.frameX = 0;
+      } else {
+        this.frameX++;
+      }
+      if (this.frame < 3) this.frameY = 0;
+      else if (this.frame < 7) this.frameY = 1;
+      else if (this.frame < 11) this.frameY = 2;
+      else this.frameY = 0;
+    }
+
+    //collision with player
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < this.radius + player.radius) {
+      handleGameOver();
+    }
+  }
+  draw() {
+    /*ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.Pi * 2);
+    ctx.fill();*/
+    ctx.drawImage(enemyImage, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x - 60, this.y - 70, this.spriteWidth / 4, this.spriteHeight / 4);
+  }
+}
+
+const enemy1 = new Enemy();
+
+function handleEnemies() {
+  enemy1.draw();
+  enemy1.update();
+}
+
+function handleGameOver() {
+  ctx.fillStyle = 'white';
+  ctx.fillText('GAME OVER, you reached score ' + score, canvas.width / 2 - canvas.width / 2.7, canvas.height / 2 - 50);
+  gameOver = true;
+}
+
 //animation loop
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleBackground();
   handleBubbles();
   player.update();
   player.draw();
-  gameFrame++;
+  handleEnemies();
   ctx.fillStyle = 'black';
-  ctx.fillText('score: ' + score, 10, 50);
-  requestAnimationFrame(animate);
+  ctx.fillText('score: ' + score, 20, canvas.width / 20);
+  gameFrame++;
+  if (!gameOver) {
+    requestAnimationFrame(animate);
+  }
 }
 
 animate();
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvasPosition = canvas.getBoundingClientRect();
+  ctx.font = canvas.width / 20 + 'px Georgia';
+  if (gameOver) {
+    animate();
+  }
+})
